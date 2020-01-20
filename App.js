a0cd37e21ab8980edaf5c0b7223c121f0546d8e9
alexgergar <alexandra.gergar@gmail.com>
2020-01-20 16:59:12 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,52 @@
+import React, {useState, useEffect} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+import {firebase} from '@react-native-firebase/auth';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import MainEventScreen from './src/screens/MainEventScreen';
+
+function App() {
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  function onChange(user) {
+    setUser(user);
+    if (initializing) {
+      setInitializing(false);
+    }
+  }
+
+  useEffect(() => {
+    const subscribe = firebase.auth().onAuthStateChanged(onChange);
+    return subscribe();
+  });
+
+  if (!user) {
+    return (
+      <View>
+        <Text>Login</Text>
+      </View>
+    );
+  }
+
+
+  return (
+    <MainEventScreen />
+  );
+};
+
+
+export default App;
---
+++ src/screens/MainEventScreen.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, SafeAreaView, Modal, Button, Alert, Dimensions } from 'react-native';
+
+export default class MainEvent extends Component {
+
+render() {
+  return (
+    <SafeAreaView>
+      <Text>Hello World</Text>
+    </SafeAreaView>
+  );
+}
+
+}
+
+const styles = StyleSheet.create({
+  topBookContainer: {
+    width: '100%',
+    height: '50%',
+  },
+});
